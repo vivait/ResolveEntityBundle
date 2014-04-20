628b42ba1e06ec08e6e36966836e7e97a8d0742c
Lewis Wright <lewis@vivait.co.uk>
2014-04-20 17:54:55 +0100

    Added override services compiler pass to bundle DI.

--- Vivait/ResolveEntityBundle/VivaitResolveEntityBundle.php
+++ Vivait/ResolveEntityBundle/VivaitResolveEntityBundle.php
@@ -2,7 +2,14 @@
 
 namespace Vivait\ResolveEntityBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Vivait\ResolveEntityBundle\DependencyInjection\Compiler\OverrideServicesCompilerPass;
 
 class VivaitResolveEntityBundle extends Bundle {
+	public function build(ContainerBuilder $container) {
+		parent::build($container);
+
+		$container->addCompilerPass(new OverrideServicesCompilerPass());
+	}
 }

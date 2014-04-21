3eb32f4dda323d9701f67c6f11a5c11ee600b61c
Lewis Wright <lewis@vivait.co.uk>
2014-04-21 12:29:34 +0100

    To stop scrutinizer moaning at me.

--- Vivait/ResolveEntityBundle/VivaitResolveEntityBundle.php
+++ Vivait/ResolveEntityBundle/VivaitResolveEntityBundle.php
@@ -2,14 +2,7 @@
 
 namespace Vivait\ResolveEntityBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Vivait\ResolveEntityBundle\DependencyInjection\Compiler\OverrideServicesCompilerPass;
 
 class VivaitResolveEntityBundle extends Bundle {
-	public function build(ContainerBuilder $container) {
-		parent::build($container);
-
-		//$container->addCompilerPass(new OverrideServicesCompilerPass());
-	}
 }
